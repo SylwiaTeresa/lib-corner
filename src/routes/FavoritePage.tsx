f96615bf7b38109cba68ddfd234d1809a4926e54
Sylwia Przystawska <sylwiateresap@gmail.com>
2025-06-19 15:03:35 +0200

    Add favoritepage

---
+++ src/routes/FavoritePage.tsx
@@ -0,0 +1,8 @@
+export default function FavoritePage() {
+    return (
+        <div>
+            <h1>My favorite books</h1>
+            <p>It is emptay right now. <br /> Fill it with wonders.</p>
+        </div>
+    );
+}
