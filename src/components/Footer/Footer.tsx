98b1e38337a55c8e9013ea4f6cbb40d5c40c925b
Sylwia Przystawska <sylwiateresap@gmail.com>
2025-06-19 15:08:59 +0200

    Add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <hr />
+            ∞ This website is developed with the help of ©OpenLibrary API
+            <br />
+            ©SylwiaTeresa ∞
+        </footer>
+    );
+};
