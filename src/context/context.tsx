f36c61934f1f626cc7c459769e7d3457a18b94c6
Sylwia Przystawska <sylwiateresap@gmail.com>
2025-06-19 14:56:02 +0200

    Add context

---
+++ src/context/context.tsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import type { Book } from "../types"
+
+type BookContextType = {
+    books: Book[];
+    loading: boolean;
+    error: string | null;
+    searchTerm: string;
+    setSearchTerm: (term: string) => void;
+};
+
+export const BookContext = createContext<BookContextType | null>(null);
+
+type BookContextkProviderProps = {
+    children: React.ReactNode;
+};
+
+export function BookProvider({ children }: Readonly<BookContextkProviderProps>) {
+    const [books, setBooks] = useState<Book[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [searchTerm, setSearchTerm] = useState(() => {
+        return localStorage.getItem("searchTerm") || " ";
+    });
+
+    useEffect(() => {
+        if (!searchTerm) return;
+
+        const fetchBooks = async () => {
+            setLoading(true);
+            try {
+                const respone = await fetch(`https://openlibrary.org/search.json?q=${encodeURIComponent(searchTerm)}`);
+                const data = await respone.json();
+                setBooks(data.docs);
+                setError(null);
+            } catch (err) {
+                setError("Could not find books");
+                setBooks([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchBooks();
+    }, [searchTerm]);
+
+    useEffect(() => {
+        localStorage.setItem("searchTerm", searchTerm);
+    }, [searchTerm]);
+
+    return (
+        <BookContext.Provider value={{ books, loading, error, searchTerm, setSearchTerm }}>
+            {children}
+        </BookContext.Provider>
+    );
+};
+
+export const useBooks = () => {
+    const context = useContext(BookContext);
+    if (!context) throw new Error("useBooks must be used in BookProvider");
+    return context;
+};
