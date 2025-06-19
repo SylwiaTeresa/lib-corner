fd43578be065665ef5172c21c432fe1efc24b007
Sylwia Przystawska <sylwiateresap@gmail.com>
2025-06-19 14:36:59 +0200

    Add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <header>
+                Stuff goes in here
+            </header>
+            <hr />
+        </>
+    );
+};
