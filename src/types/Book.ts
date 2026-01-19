dbe2979e29d4e9e75ae4abb56be12b9b37f4dbf1
Sylwia Przystawska <sylwiateresap@gmail.com>
2026-01-19 20:13:08 +0100

    feat: add Book type

---
+++ src/types/Book.ts
@@ -0,0 +1,11 @@
+export type Book = {
+  key: string;
+  title: string;
+  author_name?: string[];
+  cover_i?: number;
+  first_publish_year?: number;
+  first_sentence?: string[];
+  description?: string;
+  subject?: string[];
+  number_of_pages_median?: number;
+};
