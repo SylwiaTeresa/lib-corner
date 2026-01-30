cd28d9d34b3c042cf8f1dcdaa7f7d70c567be4b5
Sylwia Przystawska <sylwiateresap@gmail.com>
2026-01-30 11:50:33 +0100

    refactor: make BookCard reusable across pages

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -3,12 +3,14 @@ import type { Book } from "../../types/Book";
 import { useFavorites } from "../../context/FavoritesContext";
 import { BookDetailsModal } from "../BookDetailsModal/BookDetailsModal";
 import { useState } from "react";
+import { TbBooksOff } from "react-icons/tb";
 
 type BookCardProps = {
     book: Book;
+    onClick?: () => void;
 };
 
-export const BookCard = ({ book }: BookCardProps) => {
+export const BookCard = ({ book, onClick }: BookCardProps) => {
     const { favorites, dispatch } = useFavorites();
     const [isOpen, setIsOpen] = useState(false);
 
@@ -20,16 +22,19 @@ export const BookCard = ({ book }: BookCardProps) => {
         } else {
         dispatch({ type: "ADD_FAVORITE", book });
         }
-    };
-
-    const coverURL = book.cover_i 
-    ? `https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg`
-    : "https://placehold.co/600x400?text=No+Cover"; 
+    }; 
 
     return (
         <>
-            <div className="book-card" onClick={() => setIsOpen(true)}>
-                <img src={coverURL} alt={`Cover for ${book.title}`} className="CardImage" />
+            <div className="book-card" onClick={onClick}>
+                {book.cover_i ? (
+                    <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-S.jpg`} 
+                    alt={book.title} 
+                />
+                ): (
+                    <div className="fallback-icon"><TbBooksOff size={64} /></div>
+                )}
+
                 <h3>{book.title}</h3>
                 <p>{book.author_name ? book.author_name.join(", ") : "Unknown author"}</p>
                 <button onClick={toggleFavorite}>{isFavorite ? "♥" : "♡"}</button>
@@ -38,7 +43,6 @@ export const BookCard = ({ book }: BookCardProps) => {
             {isOpen && (
                 <BookDetailsModal book={book} onClose={() => setIsOpen(false)} />
             )}
-
         </>
     );
 };
